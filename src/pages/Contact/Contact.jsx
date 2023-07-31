2c2fd92ca1bfc2122ccd4c67e4ffca2f605b83cd
Benjamin CHARMES <benjamin.charmes@gmail.com>
2023-07-31 11:59:41 +0100

    Update darkmode and En/Fr in CONTACT page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -17,7 +17,10 @@ import "./Contact.scss";
 const Contact = () => {
   const appState = useAtomValue(appStateStore);
 
-  const emptyTextMessage = "This field is required";
+  const emptyTextMessage =
+    appState.language === "english"
+      ? "This field is required"
+      : "Ce champ est requis";
 
   const [email, setEmail] = useState("");
   const [emptyEmail, setEmptyEmail] = useState(null);
@@ -79,13 +82,15 @@ const Contact = () => {
     setEmptyMessage(null);
   };
 
-  return (
+  return appState.language === "english" ? (
     <div className='form-container'>
       {messageSend && (
-        <div className='success-message'>Message sent successfully!</div>
+        <div className={`success-message ${appState.theme}`}>
+          Message sent successfully!
+        </div>
       )}
       {messageNotSend && (
-        <div className='error-message'>
+        <div className={`error-message ${appState.theme}`}>
           An error occurred while sending the message.
           <br />
           <div className='second-line'>
@@ -100,6 +105,7 @@ const Contact = () => {
         <div className='sub-container'>
           <label htmlFor='email'>Email :</label>
           <input
+            className={`${appState.theme}`}
             type='email'
             id='email'
             placeholder='Enter your email'
@@ -112,6 +118,7 @@ const Contact = () => {
         <div className='sub-container'>
           <label htmlFor='subject'>Subject :</label>
           <input
+            className={`${appState.theme}`}
             type='text'
             id='subject'
             placeholder='Enter your subject'
@@ -126,6 +133,7 @@ const Contact = () => {
         <div className='sub-container'>
           <label htmlFor='message'>Message :</label>
           <textarea
+            className={`${appState.theme}`}
             id='message'
             value={message}
             placeholder='Enter your message'
@@ -138,6 +146,73 @@ const Contact = () => {
         </div>
         <div className='form-button'>
           <ButtonLight text={"Send"} onClick={handleSubmit} />
+        </div>
+      </form>
+    </div>
+  ) : (
+    <div className='form-container'>
+      {messageSend && (
+        <div className={`success-message ${appState.theme}`}>
+          Message envoyé avec succès !
+        </div>
+      )}
+      {messageNotSend && (
+        <div className={`error-message ${appState.theme}`}>
+          Une erreur s'est produite lors de l'envoi du message.
+          <br />
+          <div className='second-line'>
+            Réessayez ou contactez-moi à:{" "}
+            <a className='email-link' href='mailto:benjamin.charmes@gmail.com'>
+              benjamin.charmes@gmail.com
+            </a>
+          </div>
+        </div>
+      )}
+      <form className={`${appState.theme}`} onSubmit={handleSubmit}>
+        <div className='sub-container'>
+          <label htmlFor='email'>Email :</label>
+          <input
+            className={`${appState.theme}`}
+            type='email'
+            id='email'
+            placeholder='Saisissez votre e-mail'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          {emptyEmail ? <p className='empty-message'>{emptyEmail}</p> : null}
+        </div>
+        <div className='sub-container'>
+          <label htmlFor='subject'>Objet :</label>
+          <input
+            className={`${appState.theme}`}
+            type='text'
+            id='subject'
+            placeholder='Saisissez votre objet'
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            required
+          />
+          {emptySubject ? (
+            <p className='empty-message'>{emptySubject}</p>
+          ) : null}
+        </div>
+        <div className='sub-container'>
+          <label htmlFor='message'>Message :</label>
+          <textarea
+            className={`${appState.theme}`}
+            id='message'
+            value={message}
+            placeholder='Saisissez votre message'
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
+          {emptyMessage ? (
+            <p className='empty-message'>{emptyMessage}</p>
+          ) : null}
+        </div>
+        <div className='form-button'>
+          <ButtonLight text={"Envoyer"} onClick={handleSubmit} />
         </div>
       </form>
     </div>
